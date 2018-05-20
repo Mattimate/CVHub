be3c0a8b59104cc8ef5348caa228acc27b6bfd6b
Matt <matthew-gordon@hotmail.co.uk>
2018-05-20 19:56:39 +0100

    Separated content to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,35 @@ import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { AppRoutingModule } from "./app-routing.module";
 
 import {} from "app/service";
-import { AppComponent, HomeComponent, HeaderComponent, FooterComponent } from "app/.";
+import {
+  AppComponent,
+  HomeComponent,
+  HeaderComponent,
+  FooterComponent,
+  ContentComponent,
+  BlogComponent,
+  SkillsComponent,
+  ContactComponent,
+  VideoProfileComponent,
+  AboutMeComponent,
+  ProjectsComponent
+} from "app/.";
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
-  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    ProjectsComponent,
+    AboutMeComponent,
+    BlogComponent,
+    VideoProfileComponent,
+    ContactComponent,
+    SkillsComponent
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
