5070c24dd4046b4b89d4e8694863d5adcfd437ba
Matt <matthew-gordon@hotmail.co.uk>
2018-09-16 13:26:56 +0100

    Updated angular to 7 beta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { AppRoutingModule } from "./app-routing.module";
 
 import { MaterialModule } from "./material.module";
